a958447f8475f7a8d942db1a74cc8d530dbf1771
imamnghr <imamn208@gmail.com>
2021-10-07 21:08:38 +0700

    pharsing data

--- CLIENT SERVER/app.js
+++ CLIENT SERVER/app.js
@@ -1,20 +1,26 @@
 const express = require ('express');
-const morgan = require ('morgan');
+const morgan = require ('morgan')
+const mongoose = require ('mongoose');
+const Blog = require ('./models/blog');
+
 //express app
 const app = express();
 
-const dbURI = 'mongodb+srv://imamnghr:1234@cluster0.1aa8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// Connect to MongoDB
+const dbURI = 'mongodb+srv://imamnghr:test1234@nodetuts.1aa8c.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
-
-// listen for requirement 
-app.listen(3000);
 
 // midleware & Static file
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+
+// routes
 app.get ('/', (req, res) => {
     const blogs = [
         {title: 'Kirno Finds Pussy', snippet: 'lorem ipsum dolor sit amet consecture'},
@@ -29,6 +35,17 @@ app.get ('/about', (req, res) => {
     res.render('about', { title: 'About'}); 
 
 });
+
+// Blogs routes
+app.get ('/blogs', (req, res)=> {
+    Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+        res.render('index', {title: 'all blogs', blogs: result})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
 
 app.get('/blogs/create', (req,res)=> {
     res.render('create', { title: 'Blogs'});
